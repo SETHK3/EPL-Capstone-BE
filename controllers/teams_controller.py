ed81fd45a620f55b5ce449c0a0f70058b87439c6
Seth Gartz <threegartz@gmail.com>
2024-03-19 16:25:09 -0600

    created team add function with decorator

--- controllers/teams_controller.py
+++ controllers/teams_controller.py
@@ -4,3 +4,20 @@ from db import db
 from lib.authenticate import auth, auth_admin
 from models.teams import Teams, team_schema, teams_schema
 from util.reflection import populate_object
+
+
+@auth_admin
+def team_add(req):
+    post_data = req.form if req.form else req.json
+
+    new_team = Teams.new_team_obj()
+    populate_object(new_team, post_data)
+
+    try:
+        db.session.add(new_team)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({'message': 'unable to create new team'}), 400
+
+    return jsonify({'message': 'team created', 'result': team_schema.dump(new_team)}), 201

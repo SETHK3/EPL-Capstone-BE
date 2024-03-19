67cc8e6296e5e267e3391e19786dc389a6960396
Seth Gartz <threegartz@gmail.com>
2024-03-19 16:51:27 -0600

    added get all managers function

--- controllers/managers_controller.py
+++ controllers/managers_controller.py
@@ -21,3 +21,17 @@ def manager_add(req):
         return jsonify({'message': 'unable to create new manager'}), 400
 
     return jsonify({'message': 'manager created', 'result': manager_schema.dump(new_manager)}), 201
+
+
+@auth
+def managers_get_all():
+    try:
+        query = db.session.query(Managers).all()
+
+        if not query:
+            return jsonify({'message': 'no managers found'}), 404
+
+        else:
+            return jsonify({'message': 'managers found', 'results': managers_schema.dump(query)})
+    except:
+        return jsonify({'message': 'unable to fetch managers'}), 500

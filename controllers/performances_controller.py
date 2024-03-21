d67875477710011434e3eb20bd05d695c8db5fbd
Seth Gartz <threegartz@gmail.com>
2024-03-21 16:00:27 -0600

    get all function

--- controllers/performances_controller.py
+++ controllers/performances_controller.py
@@ -21,3 +21,17 @@ def performance_add(req):
         return jsonify({'message': 'unable to create new performance record'}), 400
 
     return jsonify({'message': 'performance record created', 'result': performance_schema.dump(new_performance)}), 201
+
+
+@auth
+def performances_get_all():
+    try:
+        query = db.session.query(Performances).all()
+
+        if not query:
+            return jsonify({'message': 'no performance records found'}), 404
+
+        else:
+            return jsonify({'message': 'performance records found', 'results': performances_schema.dump(query)})
+    except:
+        return jsonify({'message': 'unable to fetch performance records'}), 500

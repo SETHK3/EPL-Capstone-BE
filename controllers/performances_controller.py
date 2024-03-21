6af943fd8c1ebbd490f6b5d73a94bacf273094a4
Seth Gartz <threegartz@gmail.com>
2024-03-21 15:33:44 -0600

    performance add function

--- controllers/performances_controller.py
+++ controllers/performances_controller.py
@@ -4,3 +4,20 @@ from db import db
 from lib.authenticate import auth, auth_admin
 from models.performances import Performances, performance_schema, performances_schema
 from util.reflection import populate_object
+
+
+@auth_admin
+def performance_add(req):
+    post_data = req.form if req.form else req.json
+
+    new_performance = Performances.new_performance_obj()
+    populate_object(new_performance, post_data)
+
+    try:
+        db.session.add(new_performance)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({'message': 'unable to create new performance record'}), 400
+
+    return jsonify({'message': 'performance record created', 'result': performance_schema.dump(new_performance)}), 201

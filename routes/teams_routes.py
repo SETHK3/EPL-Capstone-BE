246464ea5839b5a34b8a9b36229f6b90bf06d401
Seth Gartz <threegartz@gmail.com>
2024-03-25 13:59:27 -0600

    adding teams_get_all route

--- routes/teams_routes.py
+++ routes/teams_routes.py
@@ -8,3 +8,8 @@ teams = Blueprint('teams', __name__)
 @teams.route('/team', methods=['POST'])
 def add_team():
     return controllers.team_add(request)
+
+
+@teams.route('/teams', methods=['POST'])
+def teams_get_all():
+    return controllers.teams_get_all()

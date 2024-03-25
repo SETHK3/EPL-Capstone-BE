d5822275440f960fcb4cc7fc57ea8c99dda2c2fe
Seth Gartz <threegartz@gmail.com>
2024-03-25 14:31:00 -0600

    added player add and get_all routes

--- routes/players_routes.py
+++ routes/players_routes.py
@@ -3,3 +3,13 @@ from flask import Blueprint, request
 import controllers
 
 players = Blueprint('players', __name__)
+
+
+@players.route('/player', methods=['POST'])
+def player_add():
+    return controllers.player_add(request)
+
+
+@players.route('/players', methods=['GET'])
+def players_get_all():
+    return controllers.players_get_all()

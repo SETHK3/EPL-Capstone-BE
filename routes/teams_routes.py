fe269f3608550c363b5dc3fabf76542ef57e9488
Seth Gartz <threegartz@gmail.com>
2024-03-25 13:57:51 -0600

    added team_add route

--- routes/teams_routes.py
+++ routes/teams_routes.py
@@ -3,3 +3,8 @@ from flask import Blueprint, request
 import controllers
 
 teams = Blueprint('teams', __name__)
+
+
+@teams.route('/team', methods=['POST'])
+def add_team():
+    return controllers.team_add(request)

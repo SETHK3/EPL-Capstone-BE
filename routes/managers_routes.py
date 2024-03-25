75b37bacb94c3658e79adf768e1364c30e1efd17
Seth Gartz <threegartz@gmail.com>
2024-03-25 14:58:02 -0600

    get all manager route

--- routes/managers_routes.py
+++ routes/managers_routes.py
@@ -8,3 +8,8 @@ managers = Blueprint('managers', __name__)
 @managers.route('/manager', methods=['POST'])
 def add_manager():
     return controllers.manager_add(request)
+
+
+@managers.route('/managers', methods=['GET'])
+def managers_get_all():
+    return controllers.managers_get_all()

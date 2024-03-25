1f04e43a4d343f5b4340a5b5015cc4dcb3cc83be
Seth Gartz <threegartz@gmail.com>
2024-03-25 14:53:10 -0600

    added transfer xref route

--- routes/players_routes.py
+++ routes/players_routes.py
@@ -38,3 +38,8 @@ def player_update(player_id):
 @players.route('/player/delete/<player_id>', methods=['DELETE'])
 def player_delete(player_id):
     return controllers.player_delete(request, player_id)
+
+
+@players.route('/player/team', methods=['POST'])
+def player_add_transfer():
+    return controllers.player_add_transfer(request)

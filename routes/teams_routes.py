ff062d29cb7c80592758a416d557db3a7cbd0827
Seth Gartz <threegartz@gmail.com>
2024-03-25 14:19:30 -0600

    added team update and delete routes

--- routes/teams_routes.py
+++ routes/teams_routes.py
@@ -18,3 +18,13 @@ def teams_get_all():
 @teams.route('/team', methods=['GET'])
 def team_get_by_id():
     return controllers.team_get_by_id()
+
+
+@teams.route('/team/<team_id>', methods=['PUT'])
+def team_update(team_id):
+    return controllers.team_update(request, team_id)
+
+
+@teams.route('/team/delete/<team_id>', methods=['DELETE'])
+def team_delete(team_id):
+    return controllers.team_delete(request, team_id)

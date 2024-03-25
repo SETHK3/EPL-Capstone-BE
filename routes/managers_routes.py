96e737744de8303e70743732cd7344b60cd04735
Seth Gartz <threegartz@gmail.com>
2024-03-25 14:57:40 -0600

    add manager route

--- routes/managers_routes.py
+++ routes/managers_routes.py
@@ -3,3 +3,8 @@ from flask import Blueprint, request
 import controllers
 
 managers = Blueprint('managers', __name__)
+
+
+@managers.route('/manager', methods=['POST'])
+def add_manager():
+    return controllers.manager_add(request)

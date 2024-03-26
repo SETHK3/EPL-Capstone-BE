e3799909285c737fae137e7ec1d0b2b598c7d3fa
Seth Gartz <threegartz@gmail.com>
2024-03-26 13:16:25 -0600

    created init file for routes, imported and registered blueprints

---
+++ routes/__init__.py
@@ -0,0 +1,6 @@
+from .auth_routes import auth
+from .user_routes import users
+from .teams_routes import teams
+from .managers_routes import managers
+from .players_routes import players
+from .performances_routes import performances
---
+++ util/blueprints.py
@@ -0,0 +1,10 @@
+import routes
+
+
+def register_blueprints(app):
+    app.register_blueprint(routes.auth)
+    app.register_blueprint(routes.users)
+    app.register_blueprint(routes.teams)
+    app.register_blueprint(routes.managers)
+    app.register_blueprint(routes.players)
+    app.register_blueprint(routes.performances)

7f62203fcf49ce3cbe9a3f29381587e0fa317aa1
Seth Gartz <threegartz@gmail.com>
2024-04-02 13:12:32 -0600

    updated player model fields

--- models/players.py
+++ models/players.py
@@ -9,7 +9,8 @@ class Players(db.Model):
     __tablename__ = "Players"
 
     player_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    player_name = db.Column(db.String(), nullable=False)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
     nationality = db.Column(db.String(), nullable=False)
     date_of_birth = db.Column(db.String(), nullable=False)
     position = db.Column(db.String(), nullable=False)
@@ -18,8 +19,9 @@ class Players(db.Model):
     team = db.relationship("Teams", foreign_keys='[Players.team_id]', back_populates='players')
     performance = db.relationship("Performances", foreign_keys='[Performances.player_id]', back_populates='player')
 
-    def __init__(self, player_name, nationality, date_of_birth, position, team_id):
-        self.player_name = player_name
+    def __init__(self, first_name, last_name, nationality, date_of_birth, position, team_id):
+        self.first_name = first_name
+        self.last_name = last_name
         self.nationality = nationality
         self. date_of_birth = date_of_birth
         self.position = position
@@ -31,7 +33,7 @@ class Players(db.Model):
 
 class PlayersSchema(ma.Schema):
     class Meta:
-        fields = ['player_id', 'player_name', 'nationality', 'date_of_birth', 'position', 'team', 'stats']
+        fields = ['player_id', 'first_name', 'last_name', 'nationality', 'date_of_birth', 'position', 'team', 'stats']
     team = ma.fields.Nested("TeamNameSchema")
     stats = ma.fields.Nested("PerformanceSchema", exclude=['player'])
 
